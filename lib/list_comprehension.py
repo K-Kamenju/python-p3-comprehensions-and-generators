d7f2e92b3d919fb05b3024f00492f43543613373
Caleb <kamenjuhkiiru@gmail.com>
2023-11-26 15:28:28 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [i for i in num_list if i % 2 == 0]
+    return even_numbers
+
 
 def make_exclamation(sentence_list):
-    pass
+    add_exclamation = [f"{i}!" for i in sentence_list]
+    return add_exclamation
+
